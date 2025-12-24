a3f3aed966e9c5e834b036c291af443ce49410a5
Deadsg <deadsg235@gmail.com>
2025-12-23 20:53:57 -0500

    updated vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   transpilePackages: ['three'],
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }];
